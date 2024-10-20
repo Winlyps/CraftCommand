ea8d382d9780b34767b5465db32f27eb8d0a22ad
Winlypse <nts3@o2.pl>
2024-10-20 12:57:55 +0200

    Initial commit

---
+++ src/main/kotlin/winlyps/craftCommand/CraftCommand.kt
@@ -0,0 +1,14 @@
+package winlyps.craftCommand
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CraftCommand : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/kotlin/winlyps/craftCommand/PermissionManager.kt
@@ -0,0 +1,4 @@
+package winlyps.craftCommand
+
+class PermissionManager {
+}
